089ab26c0451c89a5612d4cfa0a1bd2ffccd6cf3
jjy0709 <jjy0709@kaist.ac.kr>
2023-07-27 16:59:15 +0900

    [추가] 게시글 작성을 담당하는 controller 추가
    - qna/form.html 에서 글쓰기 버튼을 클릭할 경우 /qna/write로 요청이 가도록 하였다.
    - /qna/write 요청을 처리하는 Controller 추가

---
+++ src/main/java/webserver/controllers/QnAWriteController.java
@@ -0,0 +1,51 @@
+package webserver.controllers;
+
+import model.Session;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.controllers.annotations.RequestMethod;
+import webserver.controllers.annotations.RequestPath;
+import webserver.http.HttpRequest;
+import webserver.http.HttpResponse;
+
+import java.util.Map;
+import java.util.Set;
+
+import static service.SessionService.getSession;
+import static webserver.http.Cookie.isValidCookie;
+import static webserver.http.enums.HttpResponseStatus.BAD_REQUEST;
+
+@RequestPath(path = "/qna/write")
+public class QnAWriteController implements Controller {
+    private static final Logger logger = LoggerFactory.getLogger(QnAWriteController.class);
+
+    @RequestMethod(method = "POST")
+    public HttpResponse handlePost(HttpRequest request) {
+        if (!isValidCookie(request.cookie())) {
+            return createFoundResponse(request, "/user/login.html");
+        }
+        Map<String, String> parameters = request.body();
+
+        if (!verifyParameter(parameters)) {
+            // todo: 에러 상황에 맞게 페이지 만들어서 반환
+            return createErrorResponse(request, BAD_REQUEST);
+        }
+
+        Session session = getSession(request.cookie().getSessionId());
+        User user = session.getUser();
+
+        Post post = new Post(user, parameters.get("title"), parameters.get("contents"));
+        addPost(post);
+
+        return createFoundResponse(request, "/index.html");
+    }
+
+    private boolean verifyParameter(Map<String, String> parameters) {
+        Set<String> essentialField = Set.of("title", "contents");
+        if (!parameters.keySet().equals(essentialField)) {
+            return false;
+        }
+        return !parameters.containsValue("");
+    }
+}
